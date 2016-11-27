60ff896620ef0a820fc845b5c5a349658eab6845
nelmiawati <nelmia.wati@gmail.com>
2016-11-27 17:23:16 +0800

    cloud client

--- core/src/main/java/id/ac/polibatam/mj/dcloud/io/ICloudClient.java
+++ core/src/main/java/id/ac/polibatam/mj/dcloud/io/ICloudClient.java
@@ -1,9 +1,15 @@
 package id.ac.polibatam.mj.dcloud.io;
 
+import java.io.File;
+
 public interface ICloudClient {
 
-	void upload();
-	
-	void download();
-	
+	void upload(final File file);
+
+	void download(final File file);
+
+	void delete(final File file);
+
+	void list(final File dir);
+
 }
