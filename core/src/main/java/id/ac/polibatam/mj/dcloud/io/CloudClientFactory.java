31fab2cf2d8bf091819966fc63da368c2435bdbc
nelmiawati <nelmia.wati@gmail.com>
2016-12-04 14:45:52 +0800

    CloudClient Factory

---
+++ core/src/main/java/id/ac/polibatam/mj/dcloud/io/CloudClientFactory.java
@@ -0,0 +1,29 @@
+package id.ac.polibatam.mj.dcloud.io;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class CloudClientFactory {
+
+	public ICloudClient getCloudClien(final String className, final String accessToken) {
+
+		ICloudClient client = null;
+		try {
+			final Class<?> clazz = Class.forName(className);
+			final Constructor<?> constructor = clazz.getConstructor(String.class);
+			client = (ICloudClient) constructor.newInstance(accessToken);
+		} catch (ClassNotFoundException e) {
+
+		} catch (NoSuchMethodException e) {
+			
+		} catch (InvocationTargetException e) {
+			
+		} catch (IllegalAccessException e) {
+			
+		} catch (InstantiationException e) {
+			
+		}
+		return client;
+	}
+
+}
